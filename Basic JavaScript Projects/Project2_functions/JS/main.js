8538d91fc66661cd4bd15ba4d526be14e9f7a244
danielle-han <61366948+danielle-han@users.noreply.github.com>
2021-08-06 10:43:51 -0700

    Added new folder and files

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+//function that displays colored text in html element with id: 'color' when called.
+function textColor() {
+    var text = 'This is blue text'; //assign var text to string
+    var coloredText = text.fontcolor('blue'); //change color of string assigned to 'text' to blue.
+    document.getElementById('color').innerHTML = coloredText; //display the colored text in HTML element with id 'color'
+}
+
+//function that displays concatenated string in html element with id:'concatenate' when called.
+function onClick(){
+var sentence = "this is a string"; //assign string to var 
+sentence += " that has been concatenated"; //concatenate another string to string assigned to var
+document.getElementById('concatenate').innerHTML = sentence; //display concatenated string in HTML element with id 'concatenate'
+}
